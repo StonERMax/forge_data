04392945f804f031be7decb0526071bc8afd66da
asrafulashiq <asrafulashiq@gmail.com>
2019-05-17 14:18:38 -0400

    ffmpeg

--- video.sh
+++ video.sh
@@ -13,10 +13,10 @@ for V in $(ls $v_dir)
 do
         write_dir="./data/davis_tempered/video/${V}"
         mkdir -p $write_dir
-        ffmpeg -y -i "${v_dir}/${V}/%d.jpg" -framerate $rate \
+        ffmpeg -y -r $rate -i "${v_dir}/${V}/%d.jpg" -r 30 \
                 ${write_dir}/${V}_tempered.mp4
 
-        ffmpeg -y -i "${gt_dir}/${V}/%d.png" -framerate $rate \
+        ffmpeg -y -r $rate -i "${gt_dir}/${V}/%d.png" -r 30 \
                 ${write_dir}/${V}_tempered_gt.mp4
 done
 

f10fe04499c873983c11868220d0b5a3ec9f74e7
asrafulashiq <asrafulashiq@gmail.com>
2019-05-20 09:34:47 -0400

    added run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+
+echo ###############################################
+python -W ignore davis_synth_create.py --num 80
+
+echo ###############################################
+echo ###############################################
+
+python -W ignore segtrack_synth_create.py --num 40
+
+
+echo ###############################################
+echo ###############################################
+python -W ignore youtube_synth_create.py --num 40
+

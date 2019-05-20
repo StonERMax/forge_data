46ae2d39e203bde17f993e0a04d2082e7d188132
asrafulashiq <asrafulashiq@gmail.com>
2019-05-20 17:02:44 -0400

    /

--- video.sh
+++ video.sh
@@ -1,22 +1,25 @@
 
-rate=$1
-if [ -z $1 ]; then
-        rate=5
-fi
+Datasets=("davis_tempered" "SegTrackv2_tempered"
+        "youtube_tempered")
 
+rate=5
 echo Frame Rate: $rate
 
-v_dir="./data/davis_tempered/vid"
-gt_dir="./data/davis_tempered/gt_mask"
-
-for V in $(ls $v_dir)
+for dat in "${Datasets[@]}";
 do
-        write_dir="./data/davis_tempered/video/${V}"
-        mkdir -p $write_dir
-        ffmpeg -y -r $rate -i "${v_dir}/${V}/%d.jpg" -r 30 \
-                ${write_dir}/${V}_tempered.mp4
 
-        ffmpeg -y -r $rate -i "${gt_dir}/${V}/%d.png" -r 30 \
-                ${write_dir}/${V}_tempered_gt.mp4
+        v_dir="./data/${dat}/vid"
+        gt_dir="./data/${dat}/gt_mask"
+
+        for V in $(ls $v_dir)
+        do
+                write_dir="./data/${dat}/video/${V}"
+                mkdir -p $write_dir
+                ffmpeg -y -r $rate -i "${v_dir}/${V}/%d.png" -r 30 \
+                        ${write_dir}/${V}_tempered.mp4
+
+                ffmpeg -y -r $rate -i "${gt_dir}/${V}/%d.jpg" -r 30 \
+                        ${write_dir}/${V}_tempered_gt.mp4
+        done
+
 done
-

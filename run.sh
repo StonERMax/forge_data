ce32ce403aebee52e0980b1d724c784714583c0b
asrafulashiq <asrafulashiq@gmail.com>
2019-05-22 12:24:31 -0400

    /

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
 
-# echo ###############################################
-# python -W ignore davis_synth_create.py --num 80
+echo ###############################################
+# python -W ignore davis_synth_create.py
+echo ###############################################
+echo ###############################################
 
-# echo ###############################################
-# echo ###############################################
-
-# python -W ignore segtrack_synth_create.py --num 40
-
-
-# echo ###############################################
-# echo ###############################################
-# python -W ignore youtube_synth_create.py --num 40
+python -W ignore segtrack_synth_create.py
 
 
-cd ~/dataset/VIRAT_dataset
-python virat_anno.py
+echo ###############################################
+echo ###############################################
+python -W ignore youtube_synth_create.py
 
-cd /home/islama6a/local/forge_data
-python -W ignore virat_synth_create.py
+
+# cd ~/dataset/VIRAT_dataset
+# python virat_anno.py
+
+# cd /home/islama6a/local/forge_data
+# python -W ignore virat_synth_create.py
